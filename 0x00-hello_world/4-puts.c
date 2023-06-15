b47d0ceb75eb858b58e53c93045ff2946dd03d02
Ademola <oriolaademola@gmail.com>
2023-06-15 11:50:48 -0700

    added puts code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * print programming is like building a multilingual puzzle
+ *
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char program[100] = "Programming is like building a multilingual puzzle";
+
+	puts(program);
+
+	return (0);
+}
