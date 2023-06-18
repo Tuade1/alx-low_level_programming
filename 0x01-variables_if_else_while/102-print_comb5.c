3d6a405c9feba5369858e338efa6ec3c335355cf
Ademola <oriolaademola@gmail.com>
2023-06-18 06:37:24 -0700

    added answer for print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,32 @@
 
 int main(void)
 {
-	int a, b;
+	int i, j;
+	int a, b, c, d;
 
-	for (a = 0; a < 100; a++)
+	for (i = 0; i < 100; i++)
 	{
-		for (b = a + 1; b < 100; b++)
-		{
-			putchar('0' + (a / 10));
-			putchar('0' + (a % 10));
-			putchar(' ');
-			putchar('0' + (b / 10));
-			putchar('0' + (b % 10));
+		a = i / 10;
+		b = i % 10;
 
-			if (a < 99 || b < 99)
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10;
+			d = j % 10;
+
+			if ((a < c) || (a == c && b < d))
 			{
-				putchar(',');
+				putchar(a + '0');
+				putchar(b + '0');
 				putchar(' ');
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8 && c == 9 && d == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
