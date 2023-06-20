977657faea131eeb8a3656eb917d834ab21e20a8
Tuade1 <oriolaademola@gmail.com>
2023-06-20 23:47:47 +0100

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - return 1, 0 or -1
+ *@n: print +, 0, -
+ *Return: 1 if greater than zero, 0 if equal to zero or -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(+);
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar(-);
+		return (-1);
+	}
+}
