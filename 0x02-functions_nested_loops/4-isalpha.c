87b295425de596365308a45c93600f204b4cb30e
Tuade1 <oriolaademola@gmail.com>
2023-06-20 23:15:49 +0100

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+  * _isalpha - returns 1 or 0
+  *@c: chech is _isalpha
+  *Return: 1 if alphabetic, 0 if not alphabetic
+  */
+
+int _isalpha(int c)
+{
+	if (_isalpha(c))
+	{
+		return (1);
+	} else
+		return (0);
+}
