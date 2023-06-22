67a9cd097bd65e56f39f676b56398a8f9626e8e3
Tuade1 <oriolaademola@gmail.com>
2023-06-22 14:14:13 +0100

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - to check is alpabet is upper case or not
+ *@c: the character to be checked
+ * Return: 1 if is upper case and 0 if is not upper case
+ */
+
+int _isupper(int c);
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
