edd92c3c45b4d4ff3a0db6e7a460ffac5f16cc76
Tuade1 <oriolaademola@gmail.com>
2023-06-22 19:10:41 +0100

    added answer for more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - to print numbers 0 to 14
+ * description: to print numbers 0 to 14 on 10 lines
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
