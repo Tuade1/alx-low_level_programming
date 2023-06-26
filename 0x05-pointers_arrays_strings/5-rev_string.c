6ccc5f5f50f0c25f1df259bee080a479c0188e1b
Tuade1 <oriolaademola@gmail.com>
2023-06-26 20:56:14 +0100

    added answer for reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - to print a string in reverse
+ * @s: the pointer to the string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int len;
+
+	len = strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		s[i];
+	}
+}
