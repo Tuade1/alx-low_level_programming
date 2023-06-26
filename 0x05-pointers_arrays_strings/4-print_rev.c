48ceb2efef6d3eb5c22080aa2ad97a0e49962b47
Tuade1 <oriolaademola@gmail.com>
2023-06-26 20:15:41 +0100

    added answer for print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - to print a string in reverse
+ * @s: the pointer to the string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = '\0'; s[i] >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
