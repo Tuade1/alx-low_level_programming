73ca34e488a8d379af160ca1405b8280161de1b8
Tuade1 <oriolaademola@gmail.com>
2023-06-26 17:44:09 +0100

    added answer for strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - to count the length of a string
+ * @s: the pointer to the string
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(s);
+	return (a);
+}
