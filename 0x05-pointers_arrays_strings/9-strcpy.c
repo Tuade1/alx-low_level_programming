ddba2e4c3d3027d4e6a382d60253c478a6ddb31c
Tuade1 <oriolaademola@gmail.com>
2023-06-27 22:22:44 +0100

    added answer for strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - to copy the string pointed to by a pointer
+ * @dest: the destination
+ * @src; the pointer to the string to be copied
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
