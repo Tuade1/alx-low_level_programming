926a088ec01c5445ceefeeb7fe96ae46062b0773
Tuade1 <oriolaademola@gmail.com>
2023-06-27 17:19:57 +0100

    added answer for puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * puts2 - to print everyother character of a string,
@@ -9,12 +10,14 @@
 void puts2(char *str)
 {
 	int i;
+	int len = strlen(str);
+	int j = len - 1;
 
-	for (i = 0; i < '\0'; i++)
+	for (i = 0; i < j; i++)
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(str[i] + '0');
+			_putchar(str[i + '0']);
 		}
 	}
 	_putchar('\n');
