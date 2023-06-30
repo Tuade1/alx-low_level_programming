a1900e0de07c88e57a20e3b01d41ec789326eb0f
Tuade1 <oriolaademola@gmail.com>
2023-06-30 21:59:26 +0100

    added answer for cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,35 +10,19 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i]; i++)
 	{
-		if (i == 0)
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] -= 32;
-			}
-			continue;
+			i++;
 		}
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-			str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}' || i == 0)
 		{
-			++i;
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] -= 32;
-			}
-			continue;
+			str[i] -= 32;
 		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] += 32;
-			}
-		}
-	}
 	return (str);
 }
