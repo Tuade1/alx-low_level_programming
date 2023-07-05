1f045056321c86fe3af52b258d30957004b3c2c6
Tuade1 <oriolaademola@gmail.com>
2023-07-05 17:56:17 +0100

    added answer for puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = strlen(s);
-	if (*s > i)
+	if (*s > '\0')
 	{
 		_putchar('\n');
 	}
+	else
+	{
 		_putchar(*s);
 		_puts_recursion(++s);
+	}
 }
