4a9cfc62a47413ada77baac704b80b67d8ff28bd
Tuade1 <oriolaademola@gmail.com>
2023-07-07 23:55:30 +0100

    added answer for mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies 2 numbers
+ * @argv: the string of arguments
+ * @argc: the number of arguments passed
+ * Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
