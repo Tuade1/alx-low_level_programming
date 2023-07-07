30b27cce4eff65f5fcfcedbbb25cdd6441b6b329
Tuade1 <oriolaademola@gmail.com>
2023-07-07 23:59:12 +0100

    added answer for mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,11 +13,11 @@ int main(int argc, char *argv[])
 	if (argc < 2)
 	{
 		printf("error\n");
-		return (1);
 	}
 	else
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
-	return (0);
+	return (1);
 }
