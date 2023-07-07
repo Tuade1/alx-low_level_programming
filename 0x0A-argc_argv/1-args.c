0ad368673449031eaed44f0c25c526f49ee824fe
Tuade1 <oriolaademola@gmail.com>
2023-07-07 22:02:02 +0100

    added solution for args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the number of arguments passed into it
+ * @argv: the string of arguments
+ * @argc: the number of arguments passed
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
