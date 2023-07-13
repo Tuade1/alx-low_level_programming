12183925f3fdd8929a39edec3ea953d543a9ac27
Tuade1 <oriolaademola@gmail.com>
2023-07-13 21:29:07 +0100

    added solution to malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory
+ * @b: the size of the memory to be allocated
+ * Return: a pointer to the memory on success and a normal termination
+ * process on failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *a;
+
+	a = malloc(b);
+
+	if (a == NULL)
+	{
+		exit(1);
+	}
+
+	return (a);
+	exit(0);
+}
