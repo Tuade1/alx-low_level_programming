cd63b8747583009acf9f2993fabdc4a0338402dd
Tuade1 <oriolaademola@gmail.com>
2023-07-13 23:33:34 +0100

    added solution for calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,20 +13,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *a;
 	unsigned int i;
-	unsigned int j;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 	{
 		return (NULL);
 	}
 
-	j = nmemb * size;
-	a = malloc(j);
+	a = malloc(nmemb * size);
 	if (a == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < j; i++)
+	for (i = 0; i < nmemb; i++)
 	{
 		a[i] = 0;
 	}
