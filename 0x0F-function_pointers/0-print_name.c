ed0aef6e5f90ba1ecb30093db8b4ab672a1396b8
Tuade1 <oriolaademola@gmail.com>
2023-07-19 14:37:15 +0100

    added solution

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - function that prints a name
@@ -8,5 +9,9 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	(*f)(name);
 }
