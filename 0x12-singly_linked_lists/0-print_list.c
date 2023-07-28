f3c63807cebbd8ec226f431640121d684c68a0db
Tuade1 <oriolaademola@gmail.com>
2023-07-28 19:50:29 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,13 +20,13 @@ size_t print_list(const list_t *h)
 	while (new != NULL)
 	{
 		i++;
-		if (h->str == NULL)
+		if (new->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] [%s]\n", h->len, h->str);
+			printf("[%d] [%s]\n", new->len, new->str);
 		}
 		new = new->next;
 	}
