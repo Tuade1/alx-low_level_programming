2c46a4be9e1a09ae74e4382d93379131192a79bd
Tuade1 <oriolaademola@gmail.com>
2023-08-08 12:16:12 +0100

    task 1

---
+++ 0x15-file_io/1_create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_file - function that creates a file
+ * @filename: the name of the file
+ * @text_content: the content to be written to the file
+ * Return: 1 on success, -1 on failure and if filename is NULL
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+
+	if (filename == NULL)
+		return (-1);
+
+	fp = fopen(filename, "w");
+
+	if (fp == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		fprintf(fp, "%s", text_content);
+
+	fclose(fp);
+	return (1);
+}
