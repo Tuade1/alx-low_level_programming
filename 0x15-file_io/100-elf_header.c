a03a68f09fe90932efa793277c09c91d2df6326e
Tuade1 <oriolaademola@gmail.com>
2023-08-08 18:32:58 +0100

    new test2

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -68,3 +68,14 @@ void print_elf_header_info(const char *filename)
 
 	close(fd);
 }
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (1);
+	}
+	print_elf_header_info(argv[1]);
+	return (0);
+}
